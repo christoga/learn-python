c97b266b640d38cbc838a1f75c3336de182126de
christoga <christogaandre@gmail.com>
2015-11-01 07:40:10 +0700

    Added print-var.py

---
+++ print-var.py
@@ -0,0 +1,11 @@
+my_name = 'Andre Christoga'
+my_age = 11 # Really, this is my age
+# my_height = 150 # Centimeter
+# my_weight = 32 # Kilogram
+# my_eyes = "Brown"
+# my_teeth = "White"
+
+print "Oh, Hello there."
+print "My name is " + my_name
+print "My age is 11"
+print  "I know, too young to code?" 
