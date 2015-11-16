1395b3177e7baf46677a7a7a4ae89d2488c6f0fa
christoga <christogaandre@gmail.com>
2015-11-16 07:54:36 +0700

    Added argv.py

---
+++ argv.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "This script called", script
+print "The first variable :", first
+print "The second variable :", second
+print "The third variable :", third
